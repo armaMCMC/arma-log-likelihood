8fdbeb63af287e50f490a69429b92b6f549296c5
whit <armstrong.whit@gmail.com>
2015-10-27 12:56:08 -0400

    add schur_prod tests

--- test/simple.test.cpp
+++ test/simple.test.cpp
@@ -8,3 +8,17 @@ TEST_CASE("uniform logp") {
     REQUIRE(ans == -std::numeric_limits<double>::infinity());
   }
 }
+
+TEST_CASE("schur prod") {
+  SECTION("matrix schur prod") {
+    arma::mat x(10,10,arma::fill::randn);
+    arma::mat y(10,10,arma::fill::randn);
+    REQUIRE(arma::all(arma::vectorise(x % y  == arma::schur_prod(x,y))));
+  }
+
+  SECTION("vector schur prod") {
+    arma::vec x(10,arma::fill::randn);
+    arma::vec y(10,arma::fill::randn);
+    REQUIRE(arma::all(arma::vectorise(x % y  == arma::schur_prod(x,y))));
+  }
+}
